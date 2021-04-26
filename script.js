f74041036f503ddac49c5f3a60a6f3e692955cf0
krisbratvold <kris.bratvold@gmail.com>
2021-04-26 13:36:10 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function message(){
+    alert("Whats up!");
+    document.getElementById("1st").style.backgroundColor = "red";
+}
+function message2(){
+    alert("This is a good button.");
+    document.getElementById("2nd").style.color = "pink";
+}
+function message3(){
+    alert("This button is OK.");
+    document.getElementById("3rd").style.borderRadius = "0%";
+}
+function message4(){
+    alert("This is the best button.");
+    document.getElementById("4th").style.backgroundColor = "red";
+    document.getElementById("4th").style.border = "black solid 5px";
+    document.getElementById("4th").style.borderRadius = "0%";
+}
+window.onclick = backgroundchange;
+function backgroundchange(){
+    document.getElementsByTagName("Body")[0]
+        .style.backgroundColor = "green";
+}
